62cdd5bfd59692fc5d7d13289536ea7d823a4268
telosw <wzpscout@163.com>
2020-03-15 14:43:35 +0800

    update fileinfo.py

--- fileinfo.py
+++ fileinfo.py
@@ -2,3 +2,20 @@ import numpy as np
 import random
 from matplotlib import pyplot as plt
 import matplotlib.cm as cm
+def no_repeat(ls):
+    d=0
+    for i in range(len(ls)):
+        for j in range(len(ls)):
+            if j!=i and ls[i]==ls[j]:
+                d=1
+                return (0,i,j)
+    if d==0:
+        return (1,0,0)
+def find_way(way):
+    if no_repeat(way)[0]==1:
+        return way
+    else:
+        m=max(no_repeat(way)[1],no_repeat(way)[2])
+        n=min(no_repeat(way)[1],no_repeat(way)[2])
+        del way[n:m]
+        return find_way(way)
